e35ef4613d01943d9e0660370b06ad90bc89e8f5
Leonardo Regano <leonardo.regano@polito.it>
2022-03-23 20:50:56 +0100

    Added automated test script for netflow alerts

---
+++ rr-tool/scripts/producer.py
@@ -0,0 +1,22 @@
+import logging
+from settings import *
+
+
+class Producer:
+
+    def __init__(self, properties=None) -> None:
+        self.properties = properties
+        try:
+            # noinspection PyUnresolvedReferences
+            from confluent_kafka import Producer
+            self.kafka_producer = Producer(self.properties)
+            self.ENABLE_MOCKUP_PRODUCER = 0
+        except ImportError:
+            self.ENABLE_MOCKUP_PRODUCER = 1
+
+    def produce(self, topic, content, callback):
+        if self.ENABLE_MOCKUP_PRODUCER == 0:
+            self.kafka_producer.produce(topic, content, callback=callback)
+        else:
+            logging.info("Mockup Kafka producer: producing message to topic " + topic)
+            logging.info("Mockup Kafka producer: " + content)
