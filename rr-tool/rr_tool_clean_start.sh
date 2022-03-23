9381e7bf535d211e69e4fe422cad124f0684a2f0
Leonardo Regano <leonardo.regano@polito.it>
2022-03-23 21:03:43 +0100

    Added automated test script for netflow alerts

--- rr-tool/rr_tool_clean_start.sh
+++ rr-tool/rr_tool_clean_start.sh
@@ -17,7 +17,6 @@ echo
 
 if [ $# -eq 1 ]; then
   if [ "$1" = "netflow_test" ]; then
-    bash rr_tool_clean_start.sh
     echo "Injecting netflow alerts..."
     cd /media/palantir-nfs/ti-re/rr-tool && python3 -c "import rr_tool_helper; rr_tool_helper.inject_netflow_alerts()"
     echo "Netflow alerts injected"
