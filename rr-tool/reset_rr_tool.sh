206b2a4398a93de61a741c6ba595ff1862cf5ce7
Leonardo Regano <leonardo.regano@polito.it>
2022-03-23 19:01:27 +0100

    Added reset script

--- rr-tool/reset_rr_tool.sh
+++ rr-tool/reset_rr_tool.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /media/palantir-nfs/ti-re/rr-tool && python3 -c "import reset_iptables; flush_filtering_rules()"
+cd /media/palantir-nfs/ti-re/rr-tool && python3 -c "import reset_iptables; reset_iptables.flush_filtering_rules()"
 cd /media/palantir-nfs/ti-re && git pull && cd rr-tool && docker build -t palantir-rr-tool:1.0 . && docker tag palantir-rr-tool:1.0 10.101.10.244:5000/palantir-rr-tool:1.0 && docker push 10.101.10.244:5000/palantir-rr-tool:1.0 && kubectl delete pod rr-tool && kubectl create -f pod.yaml && sleep 5 && kubectl attach rr-tool
