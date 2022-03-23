5b73325b7f60f25a1bbb71fb1bee65b61706e3f2
Leonardo Regano <leonardo.regano@polito.it>
2022-03-23 19:26:08 +0100

    Minimize wait time for pod creation in start script

--- rr-tool/rr_tool_clean_start.sh
+++ rr-tool/rr_tool_clean_start.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+echo "Resetting iptables..."
 cd /media/palantir-nfs/ti-re/rr-tool && python3 -c "import reset_iptables; reset_iptables.flush_filtering_rules()"
+echo "Rebuilding rr-tool docker image..."
 cd /media/palantir-nfs/ti-re && git pull && cd rr-tool && docker build -t palantir-rr-tool:1.0 . && docker tag palantir-rr-tool:1.0 10.101.10.244:5000/palantir-rr-tool:1.0 && docker push 10.101.10.244:5000/palantir-rr-tool:1.0
 if [[ $(kubectl get pods --all-namespaces | grep rr-tool | wc -l) -gt 0 ]]; then
+  echo "Existing rr-tool pod found, deleting..."
   kubectl delete pod rr-tool
 fi
-kubectl create -f /media/palantir-nfs/ti-re/rr-tool/pod.yaml && sleep 5 && kubectl attach rr-tool
+echo "Creating rr-tool pod"
+kubectl create -f /media/palantir-nfs/ti-re/rr-tool/pod.yaml
+if [[ $(kubectl get pods --all-namespaces | grep rr-tool | grep Running | wc -l) -eq 0 ]]; then
+  echo -n "."
+fi
+echo "Rr-tool pod created, attaching..."
+kubectl logs rr-tool && kubectl attach rr-tool
