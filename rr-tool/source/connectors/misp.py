f8ba99758a65d32c05a0ffb799053692ecfcfef8
francescosettanni <5564178+frank7y@users.noreply.github.com>
2023-01-31 11:37:55 +0100

    test

--- rr-tool/source/connectors/misp.py
+++ rr-tool/source/connectors/misp.py
@@ -41,7 +41,7 @@ def publish_on_misp():
     misp_object.description = "foo"
     setattr(misp_object, 'meta-category', 'bar')
 
-    event.add_object(misp_object.to_json())
+    event.add_object(misp_object)
     #print(misp_object.to_json())
 
 
