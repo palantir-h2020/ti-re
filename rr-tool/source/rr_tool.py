96786ae34c0bb503f93c7b31b9097870d51fd658
francescosettanni <5564178+frank7y@users.noreply.github.com>
2023-02-23 11:10:13 +0100

    update

--- rr-tool/source/rr_tool.py
+++ rr-tool/source/rr_tool.py
@@ -176,7 +176,7 @@ class RRTool:
         logger.info("Threat report syslog: " + msg)
         json_msg = json.loads(msg)
         logger.info("Serialized syslog threat report: " + str(json_msg))
-        if json_msg.get("Threat_Category") is not "ransomware":
+        if json_msg.get("Threat_Category") != "ransomware":
             json_msg["Threat_Category"] = "unauthorized_access"
         self.jsonInput(json_msg)
 
