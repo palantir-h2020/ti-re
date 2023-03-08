659114c6d3f8d137a4d6b49f1106a5e1ee314909
francescosettanni <5564178+frank7y@users.noreply.github.com>
2023-03-08 15:48:12 +0100

    update

--- rr-tool/source/recipe_interpreter/custom_functions.py
+++ rr-tool/source/recipe_interpreter/custom_functions.py
@@ -6,7 +6,6 @@ logger = get_child_logger('recipe-interpreter', 'custom-functions')
 
 
 def fbm_call(alert, alert_source_ip):
-    logger.info('Incident response API: notifying data breach on host with IP ' + alert_source_ip)
 
     portal.notify(component_type="Recommendation and Remediation",
                   component_id="0",
@@ -17,7 +16,6 @@ def fbm_call(alert, alert_source_ip):
     ir.notify("brute-force-attack", alert_source_ip, "Data breach attempt detected: " + str(alert))
 
 def fbm_call_ransomware(alert, alert_source_ip):
-    logger.info('Incident response API: notifying data breach on host with IP ' + alert_source_ip)
 
     portal.notify(component_type="Recommendation and Remediation",
                   component_id="0",
